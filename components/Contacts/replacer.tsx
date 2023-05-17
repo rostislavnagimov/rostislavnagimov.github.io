1bb24c96c40a53bf582976868894279734ea15cc
Rostislav <rostislav.nagimov@gmail.com>
2023-05-17 15:15:14 -0700

    feat: add links replacer

--- components/Contacts/replacer.tsx
+++ components/Contacts/replacer.tsx
@@ -2,11 +2,14 @@ import email from "./email"
 import linkedIn from "./linkedin"
 import telegram from "./telegram"
 
+import ExtLink from "@/helpers/ExtLink"
+
 const mail = email({address: 'rostislav.nagimov@gmail.com'})
 const tele = telegram({name: 'rostislavnagimov'})
 const link = linkedIn({name: 'rostislavnagimov', alias: 'Rostislav Nagimov'})
 
 const replacer = (string: string) => {
+  const externalLink = string.match(/\\(.+)\\/g)
   if (string.includes('/email/')) {
     return (
       <>
@@ -28,6 +31,17 @@ const replacer = (string: string) => {
       <>
         {string.split('/linkedIn/')[0]}
         {link}
+      </>
+    )
+  }
+  if (externalLink) {
+    console.log(externalLink)
+    return (
+      <>
+        <ExtLink
+          href={externalLink[0].slice(2, -2)}
+          target='blank'
+        />
       </>
     )
   }
