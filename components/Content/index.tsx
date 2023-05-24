8ba7f387787b365f66eebe18070ae961693bcfde
Rostislav <rostislav.nagimov@gmail.com>
2023-05-24 13:55:31 -0700

    refa: lint fix

--- components/Content/index.tsx
+++ components/Content/index.tsx
@@ -7,31 +7,31 @@ import { ContentData } from './types'
 import styles from './index.module.scss'
 
 const Content: React.FC<ContentData> = ({ itemData }) => (
-  <div className={styles['Content']}>
+  <div className={styles['content']}>
     {Object.keys(itemData).map((innerIndex, index) => {
       const { label, title, content } = itemData[innerIndex]
 
       return (
-        <div className={styles['Content__item']} key={index}>
+        <div className={styles['content__item']} key={index}>
           <Row>
             {label && (
               <Col span={2}>
-                <div className={styles['Content__item__label']}>
+                <div className={styles['content__item__label']}>
                   {label}
                 </div>
               </Col>
             )}
             <Col span={label ? 10 : 12}>
-              <div className={styles['Content__item__content']}>
+              <div className={styles['content__item__content']}>
                 {title && (
-                  <div className={styles['Content__item__title']}>
+                  <div className={styles['content__item__title']}>
                     {title}
                   </div>
                 )}
                 {content.map((string, index) => (
                   <>
                     {string && (
-                      <p key={index} className={styles['Content__item__content__string']}>
+                      <p key={index} className={styles['content__item__content__string']}>
                         {replacer(string)}
                       </p>
                     )}
