b103b8a3dd732768620675be6788e122c6c253c1
Rostislav <rostislav.nagimov@gmail.com>
2023-05-27 11:55:41 -0700

    feat: implement loader

--- components/Body/index.tsx
+++ components/Body/index.tsx
@@ -1,12 +1,20 @@
 import React, { ReactNode } from 'react'
 
 import styles from './index.module.scss'
+import { useSelector } from 'react-redux'
+import { selectResume } from '@/store/slice'
 
-const Body: React.FC<BodyProps> = ({ children }) => (
-  <div className={styles['body']}>
-    {children}
+const Body: React.FC<BodyProps> = ({ children }) => {
+  const isLoading = Object.keys(useSelector(selectResume)).length ? false : true
+  
+  return (
+  <div className={styles[`body`]}>
+    {isLoading
+      ? 'Loading'
+      : children
+    }
   </div>
-)
+)}
 
 interface BodyProps {
   children: ReactNode
